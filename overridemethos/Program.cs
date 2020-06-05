19c7e8bc20d49561ed90db4958025fd079d47758
JUDY <s2018s05@e-mirim.hs.kr>
2020-06-05 09:10:09 +0900

    overrideethos

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace overridemethos
+namespace overridemethods
 {
     class Program
     {
+        static int Power(int input)
+        {
+            return input * input;
+        }
+        static int Power(int input, int count)
+        {
+            return (int)Math.Pow(input, count);
+        }
+        static int SumAll(int end)
+        {
+            int total = 0;
+
+            for (int i = 0; i < end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+        static int SumAll(int start, int end)
+        {
+            int total = 0;
+            for (int i = start; i < end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Power(2));
+            Console.WriteLine(Power(2, 3));
+            Console.WriteLine(SumAll(3));
+            Console.WriteLine(SumAll(1, 4));
+
         }
     }
 }
